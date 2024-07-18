ba13a3eae01789871870273962ee3aedbb1cafc9
Dustin Bleile <dbleile@bcgsc.ca>
2024-07-18 09:33:38 -0700

    typing errors - add 'name' as a field for records

--- pori_python/types.py
+++ pori_python/types.py
@@ -3,14 +3,13 @@ from typing import Dict, List, Optional, Tuple, TypedDict, Union
 # TODO: Can constants in inputs.py like COPY_REQ, SMALL_MUT_REQ, just be replaced by types?
 
 CategoryBaseTermMapping = List[Tuple[str, List[str]]]
-Record = TypedDict("Record", {"@rid": str, "@class": str})
+Record = TypedDict("Record", {"@rid": str, "@class": str, "name": str})
 EmbeddedRecord = TypedDict("EmbeddedRecord", {"@class": str})
 
 
 class Ontology(Record):
     sourceId: str
     sourceIdVersion: Optional[str]
-    name: str
     source: Record
     displayName: str
 
